0badc7c40e4e61dc701d886be03459a28070bee9
ayaan26 <46765524+ayaan26@users.noreply.github.com>
2022-01-24 12:25:54 -0500

    vue conf command

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,3 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/weather-project/'
-    : '/'
-}
+  publicPath: process.env.NODE_ENV === "production" ? "/weather-project/" : "/",
+};
